1030a808708fdbaaed5875d9a7744a152c4c2ba4
chry8822 <goggg8822@naver.com>
2022-02-04 18:06:55 +0900

    appleCoding React

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 /*eslint-disable*/
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import "./App.css";
 import logo from "./logo.svg";
 
-function App() {
+function App(props) {
   let [title, setTitle] = useState(["HTML", "JavaScipt", "React"]);
-  let [like, setLike] = useState(0);
   let [modal, setModal] = useState(false);
+  let [currentNum, setCurrentNum] = useState(-1);
+  let [inputResult,setInputResult] = useState('');
 
-  function changeLike() {
-    setLike(like + 1);
-  }
-  function modalOpen() {
-    setModal((modal) => !modal);
-  }
-
-  function LikeUp() {
-    return (
-    <div>
-        <span
-         className="like" 
-         onClick={()=> {changeLike}}>
-        👍
-      </span>
-      {like}
-    </div>
-    )
+  function modalOpen(titleIndex) {
+    // 현재(pushNum) 리스트가 켜져 있는 상태에서 같은리스트(i) 누르면 꺼진다.
+    // 아니면 켜져있다.
+    setModal((prevModal)=>{
+      return (
+        currentNum === titleIndex ? 
+        !prevModal 
+        : true
+      )
+    });
+    setCurrentNum(titleIndex)
   }
 
   return (
@@ -34,17 +28,19 @@ function App() {
         <div>Develope Blog</div>
       </div>
 
-      {title.map((a) => {
-        return (
-          <div className="list">
-            <h3 onClick={modalOpen}>{a}</h3>
-            <p>2월 17일 발행</p>
-            <hr />
-          </div>
-        );
-      })}
+      
+       <List title={title} modalOpen={modalOpen}/>
+   
 
-      {modal === true ? <Modal /> : null}
+      {modal === true ? <Modal title={title} currentNum={currentNum} /> : null}
+
+
+      {/* <input onChange={(e)=>{setInputResult(e.target.value)}}/> */}
+
+      <div className="publish">
+        <input onChange={(e)=>{setInputResult(e.target.value)}}/>
+        <button onClick={()=>{}}>Save</button>
+      </div>
 
       <button onClick={modalOpen}>Modal</button>
     </div>
@@ -52,14 +48,46 @@ function App() {
 }
 
 
-
-function Modal() {
-  return (
+function Modal(props) {
+   return (
     <div className="modal">
-      <h2>title</h2>
+      <h2>{props.title[props.currentNum]}</h2>
       <p>date</p>
       <p>details</p>
     </div>
+  );
+}
+
+
+function LikeUp() {
+  let [like, setLike] = useState(0);
+  function changeLike() {
+    setLike(like + 1);
+  }
+  return (
+  <div>
+      <span
+       className="like" 
+       onClick={changeLike}>
+      👍
+    </span>
+    {like}
+  </div>
+  )
+}
+
+function List({title, modalOpen}){
+  return (
+    title.map((listTitle,titleIndex)=>{
+      return(
+    <div className="list">
+      <h3 onClick={()=> modalOpen(titleIndex)}>{listTitle}</h3>
+      <LikeUp />
+      <p>2월 17일 발행</p>
+      <hr />
+    </div>
+      )
+    })
   );
 }
 
